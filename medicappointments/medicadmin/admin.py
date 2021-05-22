0738e22deb2a0140c67e35345b0bce29a719d9f0
gitname <mymail>
2021-05-22 16:20:34 +0200

    Changed site header

--- medicappointments/medicadmin/admin.py
+++ medicappointments/medicadmin/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
 
 from medicmodels.models import (
     TypeOfVisit,
@@ -9,16 +10,13 @@ from medicmodels.models import (
 )
 
 
-admin.site.register(TypeOfVisit)
-admin.site.register(Patient)
-admin.site.register(Visit)
-admin.site.register(Appointment)
+class MedicAdminSite(AdminSite):
+    site_header = 'Medics agenda administration'
 
 
-#class PatientInline(admin.TabularInline):
-#    model = Patient
 class PossibleVisitsByMedicInline(admin.TabularInline):
     model = Medic.medic_specializations.through
+    extra = 1
 
 
 class MedicAdmin(admin.ModelAdmin):
@@ -27,4 +25,11 @@ class MedicAdmin(admin.ModelAdmin):
     ]
 
 
-admin.site.register(Medic, MedicAdmin)
+admin_site = MedicAdminSite(name='medicadmin')
+
+admin_site.register(TypeOfVisit)
+admin_site.register(Patient)
+admin_site.register(Visit)
+admin_site.register(Appointment)
+
+admin_site.register(Medic, MedicAdmin)
